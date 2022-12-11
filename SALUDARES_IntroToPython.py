7fc66b903388331882e4e17511eef1ab3f62acbf
timothysaludares <111238062+timothysaludares@users.noreply.github.com>
2022-12-12 01:39:09 +0800

    Add files via upload

---
+++ SALUDARES_IntroToPython.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Student Details
+# 
+# ID Number: 215210
+# 
+# Surname: Saludares
+# 
+# Year and Course: 2 BS ITE
+
+# In[10]:
+
+
+message = "The average is"
+firstnumber = input('Enter First Number: ')
+secondnumber = input('Enter Second Number: ')
+thirdnumber = input('Enter Third Number: ')
+three_number_average = (int(firstnumber) + int(secondnumber) + int(thirdnumber)) / 3
+print(message, three_number_average)
+
+
+# In[ ]:
+
+
+
+
